16bb9fad994db59f7f3c4869479e093cf26dfd18
jiaQiu9 <351470636@qq.com>
2022-09-18 16:39:40 -0400

    modified PS1.p3; changed the exptwo

--- PS1.P3.js
+++ PS1.P3.js
@@ -3,14 +3,16 @@ const strfun= (strVal, funVal)=> funVal(strVal);
 // Source for the split: https://pineco.de/snippets/split-strings-and-keep-the-delimiter/
 
 const repfun= val=> {
-    console.log(`originalString:${val}`);
+    // console.log(`originalString:${val}`);
     // source for replace: https://dmitripavlutin.com/replace-all-string-occurrences-javascript/
     let mod= val.replaceAll('a','A');
-    console.log(`modifiedString:${mod}`);
+    // console.log(`modifiedString:${mod}`);
     // source for match() https://www.w3docs.com/snippets/javascript/how-to-count-string-occurrence-in-string.html
     let count = val.match(/a/g).length;
-    console.log(`numberReplaced:${count}`);
-    console.log(`length:${mod.length}`);
+    // console.log(`numberReplaced:${count}`);
+    // console.log(`length:${mod.length}`);
+    let result=[`originalString:${val}`,`modifiedString:${mod}`,`numberReplaced:${count}`,`length:${mod.length}`];
+    return result;
 }
 
 let expOne = strfun(
@@ -23,5 +25,6 @@ let expTwo = strfun(
     val=> repfun(val)
 )
 
-//console.log(expOne);
-expTwo;
+
+console.log(expOne);
+console.log(expTwo);

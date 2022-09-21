f2f42bee44d48ff236445e93a12954a16af1469c
jiaQiu9 <351470636@qq.com>
2022-09-21 16:59:11 -0400

    modified ps1.p3.js to look that it is using =>

--- PS1.P3.js
+++ PS1.P3.js
@@ -3,16 +3,17 @@ const strfun= (strVal, funVal)=> funVal(strVal);
 // Source for the split: https://pineco.de/snippets/split-strings-and-keep-the-delimiter/
 
 const repfun= val=> {
-    // console.log(`originalString:${val}`);
+    return [`originalString:${val}`,`modifiedString:${val.replaceAll('a','A')}`,`numberReplaced:
+    ${val.match(/a/g).length}`,`length:${val.replaceAll('a','A').length}`]
+
     // source for replace: https://dmitripavlutin.com/replace-all-string-occurrences-javascript/
-    let mod= val.replaceAll('a','A');
-    // console.log(`modifiedString:${mod}`);
+    // let mod= val.replaceAll('a','A');
+    //
     // source for match() https://www.w3docs.com/snippets/javascript/how-to-count-string-occurrence-in-string.html
-    let count = val.match(/a/g).length;
-    // console.log(`numberReplaced:${count}`);
-    // console.log(`length:${mod.length}`);
-    let result=[`originalString:${val}`,`modifiedString:${mod}`,`numberReplaced:${count}`,`length:${mod.length}`];
-    return result;
+    // let count = val.match(/a/g).length;
+    //
+    // let result=[`originalString:${val}`,`modifiedString:${mod}`,`numberReplaced:${count}`,`length:${mod.length}`];
+    // return result;
 }
 
 let expOne = strfun(
